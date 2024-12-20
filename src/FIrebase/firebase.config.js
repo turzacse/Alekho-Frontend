0b0c50f3e7b41b4e6a5eb43e893beca278087a28
Fahim Montasir Turza <turzacse@gmail.com>
2024-12-20 23:51:36 +0600

    Modified the landing page

--- src/FIrebase/firebase.config.js
+++ src/FIrebase/firebase.config.js
@@ -1,17 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBps1zO0XtrytuOtGBSKIY7T7r7LP9cdv8",
-  authDomain: "alekhophotography.firebaseapp.com",
-  projectId: "alekhophotography",
-  storageBucket: "alekhophotography.firebasestorage.app",
-  messagingSenderId: "740745999614",
-  appId: "1:740745999614:web:2fa2d4e99784848698e1af"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export default app;
